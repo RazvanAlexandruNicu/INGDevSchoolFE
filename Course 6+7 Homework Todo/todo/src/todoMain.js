f4dc8d2fbe1292c9d7a27aace7ab2ec233eca2f3
RazvanAlexandruNicu <razvanalexandrunicu@gmail.com>
2020-05-14 21:06:31 +0300

    Added location display

--- Course 6+7 Homework Todo/todo/src/todoMain.js
+++ Course 6+7 Homework Todo/todo/src/todoMain.js
@@ -9,6 +9,8 @@ export class todoMain extends LitElement {
     return {
       tasks: { type: Object },
       value: { type: String },
+      options: { type: Object },
+      location: { type: Object },
     };
   }
 
@@ -30,17 +32,62 @@ export class todoMain extends LitElement {
       todo-form {
         border-bottom: 1px solid darkblue;
       }
+
+      #address {
+        background: darkblue;
+        padding: 0.4em 1em 0.4em 1em;
+      }
     `;
+  }
+
+  async fetchLocation(crd) {
+    const response = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?lat=${crd.latitude}` +
+        `&lon=${crd.longitude}&format=json`
+    );
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+    return null;
+  }
+
+  async success(pos) {
+    var crd = pos.coords;
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+
+    const location = await this.fetchLocation(crd);
+    this.location = location;
+    console.log(this.location);
+  }
+
+  error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  getMyPosition() {
+    navigator.geolocation.getCurrentPosition(this.success.bind(this), this.error, this.options);
   }
 
   constructor() {
     super();
     this.tasks = read();
     this.value = JSON.stringify(this.tasks);
+    this.location = null;
+    this.options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+    this.getMyPosition();
   }
 
   render() {
     return html`
+      <span id="address">${this.location.display_name}</span>
       <todo-form @form-submitted="${this._onFormChange}"></todo-form>
       <todo-list @item-deleted="${this._onItemDeleted}" value=${this.value}></todo-list>
     `;
